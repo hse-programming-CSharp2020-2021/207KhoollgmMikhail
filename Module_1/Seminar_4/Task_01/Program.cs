34be1a7ae7a706860aa29a643881faf4c7bdbf83
Михаил Хооллгм <mukhoollgm@edu.hse.ru>
2020-09-24 19:30:03 +0300

    Seminar_4

---
+++ Module_1/Seminar_4/Task_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        public static bool Shift(ref char ch)
+        {
+            if (ch < 'a' || ch > 'z') return false;
+            if (ch > 'v') ch = (char)(96 + ((int)ch + 4) % 122);
+            else ch = (char)(ch + 4);
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            char ch;
+            if(!char.TryParse(Console.ReadLine(), out ch))
+            {
+                Console.WriteLine("Неверное значение");
+                return;
+            }
+
+            if (!Shift(ref ch))
+            {
+                Console.WriteLine("Неверное значение");
+                return;
+            }
+            else Console.WriteLine(ch);
+        }
+    }
+}
---
+++ Module_1/Seminar_4/Task_05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_05
+{
+    class Program
+    {
+        public static void Sum(int k)
+        {
+            double s=0;
+            for (int i = 0; i <= k; i++)
+            {
+                s += (i + 0.3) / (3 * i * i + 5);
+                Console.WriteLine($"{i} \t {s:f3}");
+            }
+            
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            int k;
+
+            int.TryParse(Console.ReadLine(), out k);
+
+            Sum(k);
+        }
+    }
+}
