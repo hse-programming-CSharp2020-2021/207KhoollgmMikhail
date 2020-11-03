c1f0732e979ae254e0741d90b66ea1ffa9cee6f4
Михаил Хооллгм <mukhoollgm@edu.hse.ru>
2020-11-03 16:10:15 +0300

    Seminar_3

---
+++ Module_2/Seminar_3/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Sin
+    {
+        public double Xmin { get; set; }
+
+        public double Xmax { get; set; }
+
+        public double delta { get; set; }
+
+        public Sin(double xmin, double xmax, double delta)
+        {
+            Xmin = xmin;
+            Xmax = xmax;
+            this.delta = delta;
+        }
+
+        public double this[double number]
+        {
+            get
+            {
+                return Math.Round(Math.Sin(number), 4);
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sin a = new Sin(0, Math.PI, Math.PI / 6);
+
+            for (double i = a.Xmin; i < a.Xmax; i += a.delta)
+            {
+                Console.WriteLine($"Sin({Math.Round(i,4)}) = {a[i]}");
+            }
+        }
+    }
+}
---
+++ Module_2/Seminar_3/Task_08/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_08
+{
+    class Program
+    {
+        public class Shedule
+        {
+            string[] week = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            public string this[int index]
+            {
+                get
+                {
+                    if (index < 1 || index > 7) return "Incorrect input";
+
+                    return week[index - 1];
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Shedule first = new Shedule();
+            for (int i = 1; i < 8; i++)
+            {
+                Console.WriteLine($"{i}. {first[i]}");
+            }
+            Console.WriteLine(first[8]);
+        }
+    }
+}
