d7ec8563a073ebfaa31b452fe18ade353ef72481
Михаил Хооллгм <mukhoollgm@edu.hse.ru>
2020-12-08 15:58:28 +0300

    Seminar_8

---
+++ Module_2/Seminar_8/Task_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static string DeleteSpaces(string a)
+        {
+            string[] str = a.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            return String.Join(' ', str);
+        }
+
+        public static int NumberOfWords(string a, int k)
+        {
+            string[] str = a.Split(" ");
+            int sum = 0;
+            foreach (string inStr in str)
+            {
+                if (inStr.Length > k) sum++;
+            }
+            return sum;
+        }
+
+        public static int Vowel(string a)
+        {
+            int sum = 0;
+            a = a.ToLower();
+            string[] str = a.Split(" ");
+            char[] vowels = { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+            foreach (string inStr in str)
+            {
+                foreach(char ch in vowels)
+                {
+                    if (inStr.StartsWith(ch)) sum++;
+                }
+            }
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string input = DeleteSpaces(Console.ReadLine());
+            Console.WriteLine(input);
+            Console.WriteLine(NumberOfWords(input, 4));
+            Console.WriteLine(Vowel(input));
+
+        }
+    }
+}
---
+++ Module_2/Seminar_8/Task_2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static string DeleteSpaces(StringBuilder a)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                if ((a[i] == ' ') && (a[i + 1] == ' '))
+                {
+                    a.Remove(i, 1);
+                    i--;
+                }                        
+            }
+            return a.ToString();
+        }
+
+        public static int NumberOfWords(string a, int k)
+        {
+            string[] str = a.Split(" ");
+            int sum = 0;
+            foreach (string inStr in str)
+            {
+                if (inStr.Length > k) sum++;
+            }
+            return sum;
+        }
+
+        public static int Vowel(string a)
+        {
+            int sum = 0;
+            a = a.ToLower();
+            string[] str = a.Split(" ");
+            char[] vowels = { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+            foreach (string inStr in str)
+            {
+                foreach (char ch in vowels)
+                {
+                    if (inStr.StartsWith(ch)) sum++;
+                }
+            }
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string input = DeleteSpaces(new StringBuilder(Console.ReadLine()));
+            Console.WriteLine(input);
+            Console.WriteLine(NumberOfWords(input, 4));
+            Console.WriteLine(Vowel(input));
+
+        }
+    }
+}
